621d1ec01f32d69c258a30a8c9a52e116fcfbfd1
Parallels <parallels@kali-linux-2022-2.localdomain>
2023-09-12 15:38:22 -0300

    Django admin config + code corrections

--- blog/admin.py
+++ blog/admin.py
@@ -3,3 +3,11 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "status", "created_on")
+    list_filter = ("status",)
+    search_fields = ("title", "post_content")
+    prepopulated_fields = {"slug": ("title", "author")}
+
+admin.site.register(Post, PostAdmin)
